9262a7e0b3b1c79f0b647bb38d1030520abfa5f2
Julian Leucker <leuckerj@gmail.com>
2022-11-15 14:35:30 +0100

    Include giftbox

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div class="snowflake-wrapper"><SnowFlakes /></div>
+  <GiftBox :open="open"/>
+  <input type="checkbox" v-model="open" @click="open = !open">
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div class="snowflake-wrapper"><SnowFlakes v-if="!reducedMotion" /></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import SnowFlakes from "@/components/SnowFlakes";
+import GiftBox from "@/components/GiftBox";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    GiftBox,
+    // HelloWorld,
     SnowFlakes,
-  }
+  },
+  data: () => {return {
+    open: true,
+    reducedMotion: true,
+  }}
 }
 </script>
 
 <style>
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+    min-width: 100vw;
+    justify-content: space-evenly;
+  }
   .snowflake-wrapper {
     height: 100vh;
     width: 100vw;
