32d7755b04aa1143cb45b887b5e4a56b6e48dab7
Julian Leucker <leuckerj@gmail.com>
2022-11-15 15:30:11 +0100

    Create GiftInput

---
+++ src/components/GiftInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+<!--    <div class="triangle"></div>-->
+    <div class="tag">
+      <input type="text" name="name-input" id="name-input" :value="modelValue"
+             @input="$emit('update:modelValue', $event.target.value)">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiftInput",
+  props: {
+    modelValue: String,
+  },
+  emits: ["update:modelValue"],
+}
+</script>
+
+<style scoped>
+.wrapper {
+  position: relative;
+}
+.tag {
+  background: #e09514;
+  height: 100px;
+  padding: 2em 2em 2em calc(2em + 25%);
+
+  -webkit-clip-path: polygon(25% 0%, 100% 0, 100% 100%, 25% 100%, 0% 50%);
+  clip-path: polygon(25% 0%, 100% 0, 100% 100%, 25% 100%, 0% 50%);
+  box-shadow: inset 0 20px 5px -12px rgba(226, 226, 226, 1);
+  border-radius: 2px;
+}
+
+input {
+  background: #ad730f;
+  border: none;
+  padding: 1em;
+  border-radius: 12px;
+  color: whitesmoke;
+  width: 100%;
+}
+</style>
