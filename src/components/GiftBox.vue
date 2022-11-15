73a8072945b81f3c663c758071f1d8224ebff488
Julian Leucker <leuckerj@gmail.com>
2022-11-15 14:34:43 +0100

    Crude giftbox

---
+++ src/components/GiftBox.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="box" :data-open="open">
+    <div class="front"></div>
+    <div class="lid"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiftBox",
+  props: {
+    open: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .box {
+    position: relative;
+    content: "";
+    aspect-ratio: 1;
+    width: clamp(300px, 75vmin, 900px);
+  }
+  .box div {
+    content: "";
+    aspect-ratio: 1;
+    /*background: red;*/
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    transition: all 2s ease;
+  }
+  .box .front {
+    width: clamp(200px, 50vmin, 600px);
+    background-image: linear-gradient(transparent 40%, green 40%, green 60%, transparent 60%),
+                      linear-gradient(to right, transparent 40%, green 40%, green 60%, transparent 60%);
+    background-color: red;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .box .lid {
+    width: clamp(220px, 55vmin, 660px);
+    height: clamp(40px, 10vmin, 120px);
+    background-image: linear-gradient(to right, transparent 37%, green 37%, green 63%, transparent 63%);
+    background-color: red;
+    top: 50%;
+    left: 50%;
+    box-shadow: 0 3px 2px -2px rgba(128, 0, 0, 1);
+    transform: translate(-50%, clamp(-330px, -27.5vmin, -110px));
+  }
+
+  .box[data-open="true"] .front {
+    transform: translate(-75%, 0%) rotate(30deg);
+  }
+
+  .box[data-open="true"] .lid {
+    transform: translate(-60%, calc(-50% + clamp(-330px, -27.5vmin, -110px))) rotate(-30deg);
+  }
+</style>
