c3b109d7e4e1adb2e656a212245bd7961fbaecaf
Julian Leucker <leuckerj@gmail.com>
2022-11-15 23:32:28 +0100

    Make box smaller

--- src/components/GiftBox.vue
+++ src/components/GiftBox.vue
@@ -20,6 +20,13 @@ export default {
       required: false,
       default: null,
     },
+  },
+  data() {
+    return {
+      min: 150,
+      vvv: 50,
+      max: 400,
+    }
   }
 }
 </script>
@@ -29,7 +36,7 @@ export default {
     position: relative;
     content: "";
     aspect-ratio: 1;
-    width: clamp(300px, 75vmin, 900px);
+    width: clamp(calc(3px / 2 * v-bind(min)), calc(3vmin / 2 * v-bind(vvv)), calc(3px / 2 * v-bind(max)));
   }
   .box div {
     content: "";
@@ -42,7 +49,7 @@ export default {
     border-radius: 2px;
   }
   .box .front {
-    width: clamp(200px, 50vmin, 600px);
+    width: clamp(calc(1px * v-bind(min)), calc(1vmin * v-bind(vvv)), calc(1px * v-bind(max)));
     background-image: linear-gradient(transparent 40%, green 40%, green 60%, transparent 60%),
                       linear-gradient(to right, transparent 40%, green 40%, green 60%, transparent 60%);
     background-color: red;
@@ -51,14 +58,14 @@ export default {
   }
 
   .box .lid {
-    width: clamp(220px, 55vmin, 660px);
-    height: clamp(40px, 10vmin, 120px);
+    width: clamp(calc(11px / 10 * v-bind(min)), calc(11vmin / 10 * v-bind(vvv)), calc(11px / 10 * v-bind(max)));;
+    height: clamp(calc(1px / 5 * v-bind(min)), calc(1vmin / 5 * v-bind(vvv)), calc(1px / 5 * v-bind(max)));
     background-image: linear-gradient(to right, transparent 37%, green 37%, green 63%, transparent 63%);
     background-color: red;
     top: 50%;
     left: 50%;
     box-shadow: 0 3px 2px -2px rgba(128, 0, 0, 1), inset 0 20px 5px -12px rgba(226, 226, 226, 1);
-    transform: translate(-50%, clamp(-330px, -27.5vmin, -110px));
+    transform: translate(-50%, clamp(calc(-11px / 20 * v-bind(max)), calc(-11vmin / 20 * v-bind(vvv)), calc(-11px / 20 * v-bind(min))));
   }
 
   .box[data-open="true"] .front {
@@ -66,7 +73,7 @@ export default {
   }
 
   .box[data-open="true"] .lid {
-    transform: translate(-60%, calc(-50% + clamp(-330px, -27.5vmin, -110px))) rotate(-30deg);
+    transform: translate(-60%, calc(-50% + clamp(calc(-11px / 20 * v-bind(max)), calc(-11vmin / 20 * v-bind(vvv)), calc(-11px / 20 * v-bind(min))))) rotate(-30deg);
   }
 
   #name {
@@ -76,7 +83,7 @@ export default {
     left: 25%;
     top: 50%;
     transform: scale(20%) rotate(-45deg);
-    font-size: clamp(40px, 10vmin, 120px);
+    font-size: clamp(calc(1px / 5 * v-bind(min)), calc(1vmin / 5 * v-bind(vvv)), calc(1px / 5 * v-bind(max)));
     transition: all ease-in-out 2s;
   }
 
